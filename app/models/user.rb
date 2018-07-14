22832246facf7640a514b54d3571c4c7df293e6c
ellreka <kozakana333@gmail.com>
2018-07-14 23:43:16 +0900

    oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  def self.find_or_create_from_auth(auth)
+    provider = auth[:provider]
+    twitter = auth[:uid]
+    name = auth[:info][:name]
+    image_url = auth[:info][:image]
+
+    self.find_or_create_by(provider: provider, twitter_id: twitter) do |user|
+      user.name = name
+      user.image_url = image_url
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :todos
   root 'todos#index'
   get '/:twitter_id' => 'todos#mypage'
+
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  root 'todos#index'
+
 
 end
