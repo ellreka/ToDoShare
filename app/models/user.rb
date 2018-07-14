363badd0fabb5f83d117f4f4fa7080d389e453ac
ellreka <kozakana333@gmail.com>
2018-07-15 00:51:40 +0900

    twitter_oauth

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  def self.find_or_create_from_auth(auth)
+    provider = auth[:provider]
+    twitter_id = auth[:uid]
+    name = auth[:info][:name]
+    image_url = auth[:info][:image]
+
+    self.find_or_create_by(provider: provider, twitter_id: twitter_id) do |user|
+      user.name = name
+      user.image_url = image_url
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+<<<<<<< Updated upstream
+=======
+  resources :todos
+  root 'todos#index'
+  get '/:twitter_id' => 'todos#mypage'
+
+
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  root 'todos#index'
+>>>>>>> Stashed changes
 end
