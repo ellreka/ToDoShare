bc097f085081c7efed6fe9f037c9d8e0ce21ef1b
akasaMac <tarou_17n1101159@nnn.ed.jp>
2018-08-09 13:00:17 +0900

    delete some code that is coment outed

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,16 +3,12 @@ class LikesController < ApplicationController
   def create
     @todo = Todo.find(params[:todo_id])
     @todo.like(current_user)
-    # like = Like.new(todo_id: params[:todo_id], twitter_id: twitter_id)
-    # like.save
     redirect_to root_path
   end
     
   def destroy
     @like = Like.find_by(todo_id: params[:todo_id])
     @like.destroy
-    # like = Like.find_by(twitter_id: twitter_id)
-    # like.destroy
     redirect_to root_path
   end
 
